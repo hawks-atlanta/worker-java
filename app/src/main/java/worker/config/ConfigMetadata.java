5ab2a4238769f22699c3bb0ff300e234ed1dbaf4
woynert <whayner.porras.2020@upb.edu.co>
2023-09-23 06:40:38 -0500

    feat: metadata service add request: mark file as ready

---
+++ app/src/main/java/worker/config/ConfigMetadata.java
@@ -0,0 +1,6 @@
+package worker.config;
+
+public class ConfigMetadata
+{
+	public static final String basePath = "/api/v1";
+}
---
+++ app/src/main/java/worker/services/MetadataService.java
@@ -0,0 +1,44 @@
+package worker.services;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+import worker.config.Config;
+import worker.config.ConfigMetadata;
+
+public class MetadataService
+{
+	public static boolean fileReady (String fileUUID, int volume)
+	{
+		JSONObject body = new JSONObject ();
+		body.put ("volume", String.valueOf (volume));
+
+		String uri = String.format (
+			"%s/%s/files/ready/%s", Config.getMetadataBaseUrl (), ConfigMetadata.basePath,
+			fileUUID);
+
+		System.out.println (uri);
+
+		// PUT request
+
+		try {
+			HttpRequest request = HttpRequest.newBuilder ()
+									  .uri (URI.create (uri))
+									  .PUT (BodyPublishers.ofString (body.toString ()))
+									  .build ();
+			HttpClient client = HttpClient.newHttpClient ();
+			HttpResponse<String> res = client.send (request, HttpResponse.BodyHandlers.ofString ());
+
+			if (res.statusCode () == 204) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace ();
+		}
+
+		return false;
+	}
+}
