91660698d9f2b6167e28ee030df06671b14e239d
woynert <whayner.porras.2020@upb.edu.co>
2023-09-26 17:51:47 -0500

    feat: MetadataService saveFile

--- app/src/main/java/worker/services/MetadataService.java
+++ app/src/main/java/worker/services/MetadataService.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.util.UUID;
 import org.json.JSONObject;
 import worker.config.Config;
 import worker.config.ConfigMetadata;
@@ -19,8 +20,6 @@ public class MetadataService
 		String uri = String.format (
 			"%s/%s/files/ready/%s", Config.getMetadataBaseUrl (), ConfigMetadata.basePath,
 			fileUUID);
-
-		System.out.println (uri);
 
 		// PUT request
 
@@ -40,5 +39,62 @@ public class MetadataService
 		}
 
 		return false;
+	}
+
+	public static boolean checkReady (String fileUUID)
+	{
+		String uri = String.format (
+			"%s/%s/files/metadata/%s", Config.getMetadataBaseUrl (), ConfigMetadata.basePath,
+			fileUUID);
+
+		// GET
+
+		try {
+			HttpResponse<String> res = HttpClient.newHttpClient ().send (
+				HttpRequest.newBuilder ().uri (URI.create (uri)).GET ().build (),
+				HttpResponse.BodyHandlers.ofString ());
+
+			if (res.statusCode () == 200) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace ();
+		}
+
+		return false;
+	}
+
+	public static UUID
+	saveFile (UUID userUUID, UUID directoryUUID, String filetype, String filename, int filesize)
+		throws Exception
+	{
+
+		JSONObject body = new JSONObject ();
+		body.put ("userUUID", userUUID);
+		body.put ("parentUUID", directoryUUID == null ? JSONObject.NULL : directoryUUID);
+		body.put ("fileType", "archive");
+		body.put ("fileName", filename);
+		body.put ("fileExtension", filetype == null ? JSONObject.NULL : filetype);
+		body.put ("fileSize", filesize);
+
+		// post
+
+		try {
+			HttpResponse<String> res = HttpClient.newHttpClient ().send (
+				HttpRequest.newBuilder ()
+					.uri (URI.create (Config.getMetadataBaseUrl () + "/api/v1/files"))
+					.POST (BodyPublishers.ofString (body.toString ()))
+					.build (),
+				HttpResponse.BodyHandlers.ofString ());
+
+			JSONObject resBody = new JSONObject (res.body ());
+			if (res.statusCode () == 201) {
+				return UUID.fromString (resBody.getString ("uuid"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace ();
+		}
+
+		throw new Exception ("Couldn't save file metadata");
 	}
 }
